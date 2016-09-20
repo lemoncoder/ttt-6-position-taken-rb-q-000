e25c78a7fb63dc78687da2b717ae204a08520304
Lisa Nelson <lisan@on74th.com>
2016-09-20 23:51:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if
+   board[index] == "X"
+    true
+  elsif
+    board[index] == "O"
+    true
+  else
+    board[index] == " " || "" || nil
+    false
+  end
+end
